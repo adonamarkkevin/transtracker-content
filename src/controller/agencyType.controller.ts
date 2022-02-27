4230b6c9b00726d086e14cecebffdcfaa48c2d13
Mark Kevin Adona <adonamarkkevin@gmail.com>
2022-02-28 03:48:00 +0800

    working on agency controller

--- src/controller/agencyType.controller.ts
+++ src/controller/agencyType.controller.ts
@@ -3,6 +3,23 @@ import { getRepository } from "typeorm";
 import { Agency } from "../entity/Agency";
 import { AgencyType } from "../entity/AgencyType";
 
+// Paginate function
+const paginateResponse = (data: any, page: any, limit: any) => {
+	const [result, total] = data;
+	const lastPage = Math.ceil(total / Number(limit));
+	const nextPage = Number(page) + 1 > lastPage ? null : Number(page) + 1;
+	const prevPage = Number(page) - 1 < 1 ? null : Number(page) - 1;
+	return {
+		data: [...result],
+		count: total,
+		perPage: Number(limit),
+		currentPage: Number(page),
+		nextPage: nextPage,
+		prevPage: prevPage,
+		lastPage: lastPage,
+	};
+};
+
 export const agencyTypeCreate = async (req: Request, res: Response) => {
 	const agencyRepo = getRepository(Agency);
 	const agencyTypeRepo = getRepository(AgencyType);
@@ -52,6 +69,26 @@ export const agencyToDelete = async (req: Request, res: Response) => {
 			.softDelete();
 
 		return res.send(`${agencyToDelete.name} deleted`);
+	} catch (error) {
+		console.error(error);
+		return res.status(500).send(`Server error`);
+	}
+};
+
+export const getAllAgencies = async (req: Request, res: Response) => {
+	try {
+		const agencyRepo = getRepository(Agency);
+		const take: any = req.query.take || 10;
+		const page: any = req.query.page || 1;
+		const skip = (page - 1) * take;
+
+		const allAgency = await agencyRepo.findAndCount({
+			relations: ["agency_type"],
+			take: take,
+			skip: skip,
+		});
+
+		return res.send(paginateResponse(allAgency, page, take));
 	} catch (error) {
 		console.error(error);
 		return res.status(500).send(`Server error`);
